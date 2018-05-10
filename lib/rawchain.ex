eca1be39ae182ab2a2b917618f23249c3e01d997
Thomas Florin <thoflo002@gmail.com>
2018-05-09 21:31:09 -0500

    Adding more comments for documentation

--- lib/rawchain.ex
+++ lib/rawchain.ex
@@ -4,6 +4,7 @@ defmodule RawChain do
   @moduledoc """
   This is a simple data store for the raw block chain
   """
+  # stores the next block in the chain, will also handle creating the timestamp and increamenting the index, and hashing the new block.
   def storeNextBlock(agent, data) do 
     currentDate = Timex.now 
     previousBlock = getCurrentBlock(agent)
@@ -13,32 +14,39 @@ defmodule RawChain do
     newBlock = createBlock(previousHash, newHash, currentDate, newIndex, data)
     Agent.update(agent, fn list -> [newBlock | list] end)
   end
-
+  
+  # A helper function for creating a tuple
   defp createBlock(previousHash, hash, date, index, data) do
     { :previousHash, previousHash, :hash, hash, :date, date, :index, index,:data, data }
   end
-
+  
+  # function for creating the hash for a new block
   def calculateHash(index, previousHash, timestamp, data) do
     {:ok, default_str} = Timex.format(timestamp, "{ISO:Extended}")
     :crypto.hash(:sha256, "#{index}#{previousHash}#{default_str}#{data}") |> Base.encode16
   end
 
+  # function for returning the last inserted  block in the chain
   def getCurrentBlock(agent) do
     Agent.get(agent, fn [head] -> head end)     
   end
-
+  
+  # function for creating a genesis block, which is the first block in the chain
   def createGenesisBlock() do 
     createBlock("0", "0", Duration.now, 0, "GENSISBLOCK")
   end
-
+  
+  # function creating a new chain
   defp createNewChain() do
     [createGenesisBlock()]
   end
 
+  # function for getting the entire chain
   def getChain(agent) do 
     Agent.get(agent, fn list -> list end)
   end
 
+  # starts a new chain with the agent
   def startChain() do
     Agent.start_link fn -> createNewChain() end
   end
--- test/rawdata_test.exs
+++ test/rawdata_test.exs
@@ -5,6 +5,7 @@ defmodule RawChainTest do
 
   test "Create a Gensis Chain" do
     { :previousHash, previousHash, :hash, hash, _, _, :index, index, :data, data} = RawChain.createGenesisBlock()
+    # verify that we are creating a genesis tuple
     assert index == 0
     assert hash == "0"
     assert previousHash == "0"
@@ -14,6 +15,7 @@ defmodule RawChainTest do
   test "Create new Chain" do
     {:ok, agent}= RawChain.startChain()
     [{:previousHash, previousHash, :hash, hash, _, _, :index, index, :data, data}] = RawChain.getChain(agent)
+    # verify that this is truely a genesis block contained in the chain
     assert index == 0
     assert hash == "0"
     assert previousHash == "0"
@@ -21,12 +23,18 @@ defmodule RawChainTest do
   end
   
   test "Add a block to a chain" do
+    # start a new chain
     {:ok, agent} = RawChain.startChain()
+    # add a new block with some data
     RawChain.storeNextBlock(agent, "somedata")
+    #get the current chain, and break it apart
     [head | tail] = RawChain.getChain(agent)
     [genesis] = tail
+    #verify that the previousHash of the latest block matches the hash of the genesis block
     assert elem(head,1) == elem(genesis,1)
+    #verify that the index got increamented
     assert elem(head, 7) == 1
+    #verify that the data was properly stored
     assert elem(head, 9) == "somedata"
       
     end
